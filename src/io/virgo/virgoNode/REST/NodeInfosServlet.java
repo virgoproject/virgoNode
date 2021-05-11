18f8600db882894dc1d91cf1154918a0a60f3986
bliz <bliz@DESKTOP-4LMDU2C>
2021-05-11 17:50:53 +0200

    Updated REST API NodeInfosServlet

--- src/io/virgo/virgoNode/REST/NodeInfosServlet.java
+++ src/io/virgo/virgoNode/REST/NodeInfosServlet.java
@@ -11,9 +11,10 @@ public class NodeInfosServlet {
 		JSONObject nodeInfosResp = new JSONObject();
 		nodeInfosResp.put("appName", Main.getAppName());
 		nodeInfosResp.put("netID", Main.getNetID());
-		nodeInfosResp.put("DAGHeight", Main.getDAG().getBestTipBeacon().getBeaconHeight());
-		nodeInfosResp.put("DAGWeight", Main.getDAG().getGenesis().getWeight());
-		nodeInfosResp.put("poolSize", Main.getDAG().getPoolSize());
+		nodeInfosResp.put("BeaconChainWeight", Main.getDAG().getGenesis().getWeight());
+		nodeInfosResp.put("BeaconChainHeight", Main.getDAG().getBestTipBeacon().getBeaconHeight());
+		nodeInfosResp.put("DAGHeight", Main.getDAG().getLoadedTx(Main.getDAG().getTipsUids().get(0)).getHeight());
+		nodeInfosResp.put("DAGSize", Main.getDAG().loadedTxsCount());
 		
 		return new Response(200, nodeInfosResp.toString());
 	}
